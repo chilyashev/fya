b0c7655c4334bdf8a00db28f3a43db54ea940b56
Mihail Chilyashev <chilyashev@gmail.com>
2017-05-01 21:08:56 +0300

    Added source

---
+++ background.js
@@ -0,0 +1,19 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details) { // onBeforeRequest callback
+        // Check if the URL being loaded contains "urm_source"
+        if (details.url.indexOf("utm_source") > -1) {
+            // Replace utm_source with something nicer
+            var new_url = details.url.replace(/utm_source\=([\w\-\.\s]+[^&#?\s]+)?/g, "utm_source=fuck_your_analytics");
+            // Tell the browser it should load the new nicer URL
+            return {redirectUrl: new_url};
+        }
+        // Otherwise, don't give a chainsaw
+        return {cancel: false};
+    },
+    {
+        // Adding the callback for all URLs
+        urls: ["<all_urls>"]
+    },
+    ["blocking"]
+);
+
