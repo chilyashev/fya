f841b81a117cb812798ddd1ce43eb54ff3f6b4f7
Mihail Chilyashev <chilyashev@gmail.com>
2017-05-01 21:17:27 +0300

    Forgot to fix the permissions. It's all good now.

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.webRequest.onBeforeRequest.addListener(
         if (details.url.indexOf("utm_source") > -1) {
             // Replace utm_source with something nicer
             var new_url = details.url.replace(/utm_source\=([\w\-\.\s]+[^&#?\s]+)?/g, "utm_source=fuck_your_analytics");
-            // Tell the browser it should load the new nicer URL
+            // Tell the browser it should load the new nice URL
             return {redirectUrl: new_url};
         }
         // Otherwise, don't give a chainsaw
